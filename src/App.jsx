7aa68df6b8aef0095dd9d7fc54b9efeb0bf8b0c8
Maarten van de Bospoort <bospoor@live.com>
2025-03-01 16:51:50 -0800

    clean up

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
 import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import LogoutButton from './components/LogoutButton'
 
 function App() {
   const [count, setCount] = useState(0)
