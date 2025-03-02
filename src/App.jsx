ce6c62880002449b55687f66f3b20802dbdbe36c
Maarten van de Bospoort <bospoor@live.com>
2025-03-01 16:46:38 -0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import { useState, useEffect } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import LogoutButton from './components/LogoutButton'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const [userData, setUserData] = useState(null)
+
+  const getMe = async () => {
+    const response = await fetch('/.auth/me');
+    const payload = await response.json();
+    console.log(payload);
+    setUserData(payload);
+  }
+
+  useEffect(() => {
+    getMe();
+  }, []);
+
+  return (
+    <>
+      <div className="card">
+        <div>
+          {userData ? (
+            <pre>{JSON.stringify(userData, null, 2)}</pre>
+          ) : (
+            <p>Loading...</p>
+          )}
+        </div>
+        <button onClick={() => {
+          window.location.href = '/.auth/logout?post_logout_redirect_uri=/test.html';
+        }}>
+          Log Out
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
